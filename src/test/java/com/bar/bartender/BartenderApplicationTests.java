363d8233cae41c30e4b76a1ffce1c001d4aea6b1
Fernel Solano <ATBBBT75qvf2J6UBhPegSZdRqppR9DD3C5C3>
2022-09-24 08:57:59 -0500

    first commit

---
+++ src/test/java/com/bar/bartender/BartenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bar.bartender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BartenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
